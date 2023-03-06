3d2a65b2821c11c571db4c1d8d311c035ada0d15
Pavlo Denysiuk <olster94@gmail.com>
2023-03-06 13:30:42 +0100

    feat: enable strict typescript rules

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -10,11 +10,11 @@ const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
+app.get('/api', (_, res) => {
   res.send({ message: 'Welcome to backend!' });
 });
 
-const port = process.env.PORT || 3333;
+const port = process.env['PORT'] ?? 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
